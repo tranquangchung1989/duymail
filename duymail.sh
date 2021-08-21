c65c250f634272495a9ffca5b0a90ec40b2c9a9f
tranquangchung1989 <82724509+tranquangchung1989@users.noreply.github.com>
2021-08-21 17:57:09 +0800

    Update duymail.sh

--- duymail.sh
+++ duymail.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 POOL=us1.ethermine.org:4444
-WALLET=0xcdbccced724727b077d6554e85ddfd8465a523a2.duymail
+WALLET=0xcdbccced724727b077d6554e85ddfd8465a523a2.$(echo "$(curl -s ifconfig.me)" | tr . _ )-shop-ruby
 
 cd "$(dirname "$0")"
 
-chmod +x ./duymail && sudo ./duymail --algo ETHASH --pool $POOL --user $WALLET  $@
+chmod +x ./lolMiner && sudo ./lolMiner --algo ETHASH --pool $POOL --user $WALLET  $@
